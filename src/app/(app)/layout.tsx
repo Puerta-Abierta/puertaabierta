25cf1b553d4eca0b83c40cfad0395003c7f4a6eb
Niksed1 <image22061@gmail.com>
2025-09-10 15:54:19 -0700

    restructured the project

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Ubuntu, Lato } from "next/font/google";
 import Link from 'next/link'
-import "./globals.css";
+import "../globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
